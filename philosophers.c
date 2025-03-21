b44e8f16fee4aec7836760c2105c2710ecd40b4e
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-03-21 12:09:59 +0100

    + | Init du projet (.h, main)

---
+++ philosophers.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbastard <jbastard@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 09:35:08 by jbastard          #+#    #+#             */
+/*   Updated: 2025/03/21 12:09:15 by jbastard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int main(int ac, char **av)
+{
+	t_data data;
+
+	if (ac != 5 && ac != 6)
+		return (printf("Error: wrong number of arguments\n"));
+	return (0);
+}
---
+++ philosophers.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbastard <jbastard@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 09:35:08 by jbastard          #+#    #+#             */
+/*   Updated: 2025/03/21 10:01:46 by jbastard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_data d_data
+
+struct s_data {
+
+};
+
+#endif
