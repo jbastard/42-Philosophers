a637f5cba93aec1b4884e3a31a96b03c3508f0db
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-04-02 12:22:20 +0200

    + | Normi ok, sauf la fonction du monitor

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exécute le programme et enregistre la sortie
-./philo 5 800 200 200 7 > output.log
-
-# Compte le nombre de fois où "x is eating" apparaît
-grep -oE '[0-9]+ : is eating' output.log | awk '{count[$1]++} END {for (philo in count) print "Philosophe " philo " a mangé " count[philo] " fois."}' | sort -n -k2
