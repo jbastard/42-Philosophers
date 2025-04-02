478ab58c669e78c8a0461fb7793fb7a73059dde3
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-04-02 12:39:38 +0200

    Last commit : Rendu

--- srcs/core/monitor.c
+++ srcs/core/monitor.c
@@ -6,39 +6,48 @@
 /*   By: jbastard <jbastard@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 15:52:15 by jbastard          #+#    #+#             */
-/*   Updated: 2025/04/02 12:15:20 by jbastard         ###   ########.fr       */
+/*   Updated: 2025/04/02 12:27:08 by jbastard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philosophers.h"
 
+int	philo_did_die(t_dp *dp)
+{
+	int		i;
+	long	time;
+
+	i = 0;
+	while (i < dp->dt.philo_count)
+	{
+		time = get_time_in_ms() - dp->ph[i].meal_l;
+		if ((dp->dt.m_count < 0 || dp->ph[i].meals_nb < dp->dt.m_count)
+			&& time > dp->dt.die_t)
+		{
+			print_status(&dp->ph[i], PHILO_DIE);
+			dp->dt.stop = 1;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 void	*global_monitor(void *arg)
 {
 	t_dp	*dp;
 	int		i;
-	long	time;
 
 	dp = (t_dp *)arg;
 	while (!dp->dt.stop)
 	{
+		if (philo_did_die(dp))
+			return (NULL);
 		i = 0;
-		while (i < dp->dt.philo_count)
-		{
-			time = get_time_in_ms() - dp->ph[i].meal_l;
-			if ((dp->dt.m_count < 0 || dp->ph[i].meals_nb < dp->dt.m_count)
-				&& time > dp->dt.die_t)
-			{
-				print_status(&dp->ph[i], PHILO_DIE);
-				dp->dt.stop = 1;
-				return (NULL);
-			}
-			i++;
-		}
 		if (dp->dt.m_count > 0 && dp->dt.nb_of_meals >= dp->dt.philo_count)
 		{
 			dp->dt.stop = 1;
-			printf("%s\n", PHILO_FULL);
-			return (NULL);
+			return (printf("%s\n", PHILO_FULL), NULL);
 		}
 		usleep(100);
 	}
